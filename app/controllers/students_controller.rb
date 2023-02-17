0834838c74bd41f04adcaadcefcd40cd0c38d335
trevor dewindt <trevor.dewindt@gmail.com>
2023-02-16 20:00:10 -0700

    commit tree 00b8a2e240b9c7b111dc116c4d30ff0563f751e8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params.has_key?("name")
+      students = Student.where("first_name = '#{params[:name].camelize}' or last_name = '#{params[:name].camelize}'")
+    else 
+      students = Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
